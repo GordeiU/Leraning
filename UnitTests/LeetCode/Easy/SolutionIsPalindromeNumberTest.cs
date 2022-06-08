b17f1017b7e80ab5ff04dfcb5fcf9ca35f20638d
GordeiU <gordey.usachev@gmail.com>
2022-06-08 20:48:38 +0100

    Problem Convert Roman Integer
    - Removed some errors that were introduced by coping the class

--- UnitTests/LeetCode/Easy/SolutionIsPalindromeNumberTest.cs
+++ UnitTests/LeetCode/Easy/SolutionIsPalindromeNumberTest.cs
@@ -4,47 +4,11 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Learning.UnitTests.LeetCode.Easy {
     [TestClass]
     public class SolutionIsPalindromeNumberTest {
-        private SolutionIsPalindromeNumber TestingClass { get; set; } = default!;
+        private SolutionConvertRomanNumber TestingClass { get; set; } = default!;
 
         [TestInitialize]
         public void TestInitialize() {
-            TestingClass = new SolutionIsPalindromeNumber();
-        }
-
-        [TestMethod]
-        public void IsPalindrome_NegativeNumber_False() {
-            bool actual = TestingClass.IsPalindrome(-123);
-            Assert.IsFalse(actual);
-        }
-
-        [TestMethod]
-        public void IsPalindrome_PositiveOddNumberCount_False() {
-            bool actual = TestingClass.IsPalindrome(123);
-            Assert.IsFalse(actual);
-        }
-
-        [TestMethod]
-        public void IsPalindrome_PositiveOddNumberCount_True() {
-            bool actual = TestingClass.IsPalindrome(121);
-            Assert.IsTrue(actual);
-        }
-
-        [TestMethod]
-        public void IsPalindrome_Zero_True() {
-            bool actual = TestingClass.IsPalindrome(0);
-            Assert.IsTrue(actual);
-        }
-
-        [TestMethod]
-        public void IsPalindrome_PositiveEvenNumberCount_True() {
-            bool actual = TestingClass.IsPalindrome(1221);
-            Assert.IsTrue(actual);
-        }
-
-        [TestMethod]
-        public void IsPalindrome_PositiveEvenNumberCount_False() {
-            bool actual = TestingClass.IsPalindrome(123421);
-            Assert.IsFalse(actual);
+            TestingClass = new SolutionConvertRomanNumber();
         }
     }
 }
