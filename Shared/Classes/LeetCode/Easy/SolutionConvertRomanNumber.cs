5e67bb70aac66952845937991cf935af408182ba
GordeiU <gordey.usachev@gmail.com>
2022-06-08 23:12:34 +0100

    Update SolutionConvertRomanNumber.cs
    - Made class static as it makes sense in this context

--- Shared/Classes/LeetCode/Easy/SolutionConvertRomanNumber.cs
+++ Shared/Classes/LeetCode/Easy/SolutionConvertRomanNumber.cs
@@ -1,7 +1,44 @@
-namespace Learning.Shared.Classes.LeetCode.Easy {
-    public class SolutionConvertRomanNumber {
+using System.Collections.Generic;
 
-        public SolutionConvertRomanNumber() {
+namespace Learning.Shared.Classes.LeetCode.Easy {
+    public static class SolutionConvertRomanNumber {
+        private static readonly IReadOnlyDictionary<char, int> RomanIntegerValues = new Dictionary<char, int> {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000}
+            };
+
+        public static int ConvertRomanNumber(string number) {
+            int result = 0;
+            int idx;
+
+            for (idx = 0; idx < number.Length - 1; idx++) {
+                if (RomanIntegerValues[number[idx]] < RomanIntegerValues[number[idx + 1]]) {
+                    result += RomanIntegerValues[number[idx + 1]] - RomanIntegerValues[number[idx]];
+                    idx++;
+                }
+
+                if (RomanIntegerValues[number[idx]] == RomanIntegerValues[number[idx + 1]]) {
+                    result += (RomanIntegerValues[number[idx]] * 2);
+                    idx++;
+                }
+
+                if (RomanIntegerValues[number[idx]] > RomanIntegerValues[number[idx + 1]]) {
+                    result += RomanIntegerValues[number[idx]];
+                }
+
+                idx++;
+            }
+
+            if (idx < number.Length) {
+                result += RomanIntegerValues[number[idx]];
+            }
+
+            return result;
         }
     }
 }
