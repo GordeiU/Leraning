8a9ba7ebe39daa8775cc2b3d92f2b0e53f068ab0
GordeiU <gordey.usachev@gmail.com>
2022-06-10 23:58:17 +0100

    Parentheses Matching
    - Added implementation of the algorithm
    - TODO Figure out if it's possible to do with the numerical values of the parenthesis instead of the HashSet

--- Shared/Classes/LeetCode/Easy/ ValidParenthesesSolution.cs
+++ Shared/Classes/LeetCode/Easy/ ValidParenthesesSolution.cs
@@ -4,19 +4,36 @@ using Learning.Shared.Classes.Helpers;
 namespace Learning.Shared.Classes.LeetCode.Easy {
     public class ValidParenthesesSolution {
         private static readonly IReadOnlyDictionary<char, char> MatchingParenthesis = new Dictionary<char, char> {
-                {'{', '}'},
-                {'[', ']'},
-                {'(', ')'},
+                {'}','{'},
+                { ']', '['},
+                {')', '('},
             };
+        private static readonly HashSet<char> OpeningParentheses = new HashSet<char> { '{', '[', '(' };
         private Stack stack;
 
         public ValidParenthesesSolution() {
             stack = new Stack();
         }
 
-        public static IReadOnlyDictionary<char, char> MatchingParenthesis1 => MatchingParenthesis;
+        public bool IsValid(string brackets) {
+            foreach (char bracket in brackets) {
+                if (OpeningParentheses.Contains(bracket)) {
+                    stack.Push(bracket);
+                } else {
+                    if (stack.Count == 0) {
+                        return false;
+                    }
 
-        public bool IsValid(string s) {
+                    if ((char)stack.Pop()! != MatchingParenthesis[bracket]) {
+                        return false;
+                    }
+                }
+            }
+
+            if (stack.Count > 0) {
+                return false;
+            }
+
             return true;
         }
     }
