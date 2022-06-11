75a48a919dc86ebe5daa7d251f97dbf064705483
GordeiU <gordey.usachev@gmail.com>
2022-06-11 21:59:07 +0100

    Day 5 Remove Duplicates inside integer array in-place
    - Fleshed out the solution

--- Shared/Classes/LeetCode/Easy/RemoveDuplicatesInPlaceSolution.cs
+++ Shared/Classes/LeetCode/Easy/RemoveDuplicatesInPlaceSolution.cs
@@ -1,9 +1,16 @@
 namespace Learning.Shared.Classes.LeetCode.Easy {
     public class RemoveDuplicatesInPlaceSolution {
         public int RemoveDuplicates(int[] numbers) {
-            int idx = 0;
+            int masterIdx = 0;
 
-            return idx + 1;
+            for (int i = 0; i < numbers.Length; i++) {
+                if (numbers[masterIdx] < numbers[i]) {
+                    masterIdx++;
+                    numbers[masterIdx] = numbers[i];
+                }
+            }
+
+            return masterIdx + 1;
         }
     }
 }
