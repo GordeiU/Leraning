52918748f681e37af815198838359971a4c1066c
GordeiU <gordey.usachev@gmail.com>
2022-06-11 21:07:16 +0100

    Day 5 MergeTwoSortedLinkedLists
    - Fleshed out the solution (actual implementation of the algorithm)

--- Shared/Classes/LeetCode/Easy/MergeTwoSortedLinkedListsSolution.cs
+++ Shared/Classes/LeetCode/Easy/MergeTwoSortedLinkedListsSolution.cs
@@ -4,6 +4,43 @@ namespace Learning.Shared.Classes.LeetCode.Easy {
     public class MergeTwoSortedLinkedListsSolution {
         public ListNode? MergeTwoLists(ListNode? firstListCurrent, ListNode? secondListCurrent) {
             ListNode? root;
+
+            if (firstListCurrent == null && secondListCurrent == null) {
+                root = null;
+            } else if (firstListCurrent == null) {
+                root = secondListCurrent;
+                secondListCurrent = secondListCurrent!.next;
+            } else if (secondListCurrent == null) {
+                root = firstListCurrent;
+                firstListCurrent = firstListCurrent!.next;
+            } else if (firstListCurrent!.val <= secondListCurrent!.val) {
+                root = firstListCurrent;
+                firstListCurrent = firstListCurrent.next;
+            } else {
+                root = secondListCurrent;
+                secondListCurrent = secondListCurrent!.next;
+            }
+
+            ListNode? cur = root;
+
+            while (firstListCurrent != null || secondListCurrent != null) {
+                if (firstListCurrent == null) {
+                    cur!.next = secondListCurrent;
+                    secondListCurrent = secondListCurrent!.next;
+                } else if (secondListCurrent == null) {
+                    cur!.next = firstListCurrent;
+                    firstListCurrent = firstListCurrent!.next;
+                } else if (firstListCurrent.val <= secondListCurrent.val) {
+                    cur!.next = firstListCurrent;
+                    firstListCurrent = firstListCurrent.next;
+                } else {
+                    cur!.next = secondListCurrent;
+                    secondListCurrent = secondListCurrent.next;
+                }
+
+                cur = cur.next;
+            }
+
             return root;
         }
     }
