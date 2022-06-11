625a6006688adc58ef8d21e657d26a35a1b4cd17
GordeiU <gordey.usachev@gmail.com>
2022-06-11 21:53:48 +0100

    Day 5 Remove Duplicates inside integer array in-place
    - Added signature of the solution class
    - Added unit test for the solution

---
+++ Shared/Classes/LeetCode/Easy/RemoveDuplicatesInPlaceSolution.cs
@@ -0,0 +1,9 @@
+namespace Learning.Shared.Classes.LeetCode.Easy {
+    public class RemoveDuplicatesInPlaceSolution {
+        public int RemoveDuplicates(int[] numbers) {
+            int idx = 0;
+
+            return idx + 1;
+        }
+    }
+}
---
+++ UnitTests/LeetCode/Easy/RemoveDuplicatesInPlaceSolutionTest.cs
@@ -0,0 +1,32 @@
+using Learning.Shared.Classes.LeetCode.Easy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Learning.UnitTests.LeetCode.Easy {
+    [TestClass]
+    public class RemoveDuplicatesInPlaceSolutionTest {
+        private RemoveDuplicatesInPlaceSolution TestingClass { get; set; } = default!;
+
+        [TestInitialize]
+        public void TestInitialize() {
+            TestingClass = new RemoveDuplicatesInPlaceSolution();
+        }
+
+        [TestMethod]
+        public void RemoveDuplicates_ShortTestOne() {
+            int[] numbers = new int[] { 1, 1, 2 };
+            int actual = TestingClass.RemoveDuplicates(numbers);
+            int expected = 2;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindIfTwoComplementsExist_ShortTestTwo() {
+            int[] numbers = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int actual = TestingClass.RemoveDuplicates(numbers);
+            int expected = 5;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
