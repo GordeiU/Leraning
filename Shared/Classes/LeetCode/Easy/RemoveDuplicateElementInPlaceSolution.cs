a5b0cb4181c031ff3c818513bf8ae8affd46f1ac
GordeiU <gordey.usachev@gmail.com>
2022-06-12 13:24:29 +0100

    Day 6 RemoveDuplicateElementInPlaceSolution
    - Added skeleton of the solution class for removing the target element while preserving the order and shifting every element
    - Added unit tests for the solution

---
+++ Shared/Classes/LeetCode/Easy/RemoveDuplicateElementInPlaceSolution.cs
@@ -0,0 +1,12 @@
+namespace Learning.Shared.Classes.LeetCode.Easy {
+    /// <summary>
+    /// Class <c>RemoveDuplicateElementInPlaceSolution</c> Solution to the: https://leetcode.com/problems/remove-element/
+    /// </summary>
+    public class RemoveDuplicateElementInPlaceSolution {
+        public int RemoveElement(int[] numbers, int val) {
+            int masterIdx = 0;
+
+            return masterIdx + 1;
+        }
+    }
+}
---
+++ UnitTests/LeetCode/Easy/RemoveDuplicateElementInPlaceSolutionTest.cs
@@ -0,0 +1,32 @@
+using Learning.Shared.Classes.LeetCode.Easy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Learning.UnitTests.LeetCode.Easy {
+    [TestClass]
+    public class RemoveDuplicateElementInPlaceSolutionTest {
+        private RemoveDuplicateElementInPlaceSolution TestingClass { get; set; } = default!;
+
+        [TestInitialize]
+        public void TestInitialize() {
+            TestingClass = new RemoveDuplicateElementInPlaceSolution();
+        }
+
+        [TestMethod]
+        public void RemoveDuplicates_ShortTestOne() {
+            int[] numbers = new int[] { 3, 2, 2, 3 };
+            int actual = TestingClass.RemoveElement(numbers, 3);
+            int expected = 2;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindIfTwoComplementsExist_ShortTestTwo() {
+            int[] numbers = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            int actual = TestingClass.RemoveElement(numbers, 2);
+            int expected = 5;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
