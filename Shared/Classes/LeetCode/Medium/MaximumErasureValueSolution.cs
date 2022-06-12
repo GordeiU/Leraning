e88a7426af11129432ff15e0e6c0a9c118d16158
GordeiU <gordey.usachev@gmail.com>
2022-06-12 23:04:14 +0100

    Day 6 Maximum Erasure Value
    - Valid solution, but it's not really optimised and hits the execution limit

--- Shared/Classes/LeetCode/Medium/MaximumErasureValueSolution.cs
+++ Shared/Classes/LeetCode/Medium/MaximumErasureValueSolution.cs
@@ -1,12 +1,37 @@
+using System.Collections.Generic;
+
 namespace Learning.Shared.Classes.LeetCode.Medium {
     /// <summary>
     /// Class <c>MaximumErasureValueSolution</c> Solution to the: https://leetcode.com/problems/maximum-erasure-value/
     /// </summary>
     public class MaximumErasureValueSolution {
-        public int MaximumUniqueSubArray(int[] numbers) {
-            //TODO: Add algorithm implementation
+        public int HighestScore { get; private set; }
 
-            return 0;
+        public int MaximumUniqueSubArray(int[] numbers) {
+            Dictionary<int, int> memory = new Dictionary<int, int>();
+            int intermediateHighest = 0;
+            int highest = intermediateHighest;
+
+            for (int idx = 0; idx < numbers.Length; idx++) {
+                if (memory.ContainsKey(numbers[idx])) {
+                    intermediateHighest = 0;
+
+                    idx = memory[numbers[idx]];
+                    memory.Clear();
+
+                    continue;
+                } else {
+                    memory[numbers[idx]] = idx;
+                }
+
+                intermediateHighest += numbers[idx];
+
+                if (intermediateHighest > highest) {
+                    highest = intermediateHighest;
+                }
+            }
+
+            return highest;
         }
     }
 }
