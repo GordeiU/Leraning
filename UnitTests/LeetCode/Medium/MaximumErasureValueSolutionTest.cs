f16e375311821c9aa4e451916f1fb67c4b0eb7f1
GordeiU <gordey.usachev@gmail.com>
2022-06-12 23:02:15 +0100

    Day 6 Maximum Erasure Value
    - Added one more test case

--- UnitTests/LeetCode/Medium/MaximumErasureValueSolutionTest.cs
+++ UnitTests/LeetCode/Medium/MaximumErasureValueSolutionTest.cs
@@ -14,7 +14,6 @@ namespace Learning.UnitTests.LeetCode.Easy {
         [TestMethod]
         public void MaximumUniqueSubArray_ShortOne() {
             int[] numbers = new int[] { 4, 2, 4, 5, 6 };
-
             int actual = TestingClass.MaximumUniqueSubArray(numbers);
             int expected = 17;
             Assert.AreEqual(expected, actual);
@@ -26,6 +25,15 @@ namespace Learning.UnitTests.LeetCode.Easy {
 
             int actual = TestingClass.MaximumUniqueSubArray(numbers);
             int expected = 8;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MaximumUniqueSubArray_ShortThree() {
+            int[] numbers = new int[] { 3, 2, 8, 2, 5, 2, 1, 2, 5 };
+
+            int actual = TestingClass.MaximumUniqueSubArray(numbers);
+            int expected = 15;
             Assert.AreEqual(expected, actual);
         }
     }
