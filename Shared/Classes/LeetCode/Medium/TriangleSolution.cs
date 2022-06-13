f98fb488abf5dcbd05c9b9b92cbd807fe4b692b9
GordeiU <gordey.usachev@gmail.com>
2022-06-13 11:48:03 +0100

    Day 7 Triangle
    - Added implementation of the algorithm

--- Shared/Classes/LeetCode/Medium/TriangleSolution.cs
+++ Shared/Classes/LeetCode/Medium/TriangleSolution.cs
@@ -6,9 +6,17 @@ namespace Learning.Shared.Classes.LeetCode.Medium {
     /// </summary>
     public class TriangleSolution {
         public int MinimumTotal(List<List<int>> triangle) {
-            int result = 0;
+            for (int layerIdx = triangle.Count - 1; layerIdx > 0; layerIdx--) {
+                for (int bottomIdx = 0; bottomIdx < triangle[layerIdx].Count - 1; bottomIdx++) {
+                    if (triangle[layerIdx][bottomIdx] <= triangle[layerIdx][bottomIdx + 1]) {
+                        triangle[layerIdx - 1][bottomIdx] += triangle[layerIdx][bottomIdx];
+                    } else {
+                        triangle[layerIdx - 1][bottomIdx] += triangle[layerIdx][bottomIdx + 1];
+                    }
+                }
+            }
 
-            return result;
+            return triangle[0][0];
         }
     }
 }
