c3e44ef2d329f1f86ac0fa4c0b89144d9b6ee10e
GordeiU <gordey.usachev@gmail.com>
2022-06-13 15:01:10 +0100

    Day 7 Implement Sub String function
    - Added implementation of the algorithm

--- Shared/Classes/LeetCode/Easy/ImplementFindStrSolution.cs
+++ Shared/Classes/LeetCode/Easy/ImplementFindStrSolution.cs
@@ -1,10 +1,41 @@
 using System;
 
 namespace Learning.Shared.Classes.LeetCode.Easy {
+    /// <summary>
+    /// Class <c>ImplementFindStrSolution</c> Solution to the: https://leetcode.com/problems/implement-strstr/
+    /// </summary>
     public class ImplementFindStrSolution {
         public int StrStr(string searchString, string subString) {
-            //TODO: Implement algorithm
-            throw new NotImplementedException();
+            if (subString == string.Empty) {
+                return 0;
+            }
+
+            int intermediateIdx = 0;
+            int intermediateSubstringIdx = 0;
+
+            for (int idx = 0; idx < searchString.Length; idx++) {
+                if (searchString[idx] == subString[intermediateSubstringIdx]) {
+                    intermediateIdx = idx;
+
+                    while (searchString[intermediateIdx] == subString[intermediateSubstringIdx]) {
+                        if (intermediateIdx == searchString.Length) {
+                            return -1;
+                        }
+
+                        if (intermediateSubstringIdx +  1 == subString.Length) {
+                            return idx;
+                        }
+
+                        intermediateIdx++;
+                        intermediateSubstringIdx++;
+                    }
+
+                    idx = intermediateIdx;
+                    intermediateSubstringIdx = 0;
+                }
+            }
+
+            return -1;
         }
     }
 }
