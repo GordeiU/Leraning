21550e2e47a7dfbde7bf8c66c9579b165963cebe
GordeiU <gordey.usachev@gmail.com>
2022-06-14 22:17:20 +0100

    Day 8 Delete Operation Two Strings
    - Added the Longest Common Sequence (LCS) algorithmic solution to the problem

--- Shared/Classes/LeetCode/Medium/DeleteOperationTwoStringsSolution.cs
+++ Shared/Classes/LeetCode/Medium/DeleteOperationTwoStringsSolution.cs
@@ -6,14 +6,29 @@ namespace Learning.Shared.Classes.LeetCode.Medium {
     /// </summary>
     public class DeleteOperationTwoStringsSolution {
         public int MinDistance(string wordOne, string wordTwo) {
+            //TODO: Optimize with the dynamic programing approach
             int longestSubsequenceCount = LongestStringSubsequence(wordOne, wordTwo);
 
             return wordOne.Length + wordTwo.Length - longestSubsequenceCount * 2;
         }
 
         private int LongestStringSubsequence(string wordOne, string wordTwo) {
-            //TODO: Implement longest string subsequence recursive? algorithm
-            throw new NotImplementedException();
+            int wordOneLength = wordOne.Length;
+            int wordTwoLength = wordTwo.Length;
+
+            if (wordOneLength == 0 || wordTwoLength == 0) {
+                return 0;
+            } else if (wordOne[wordOneLength - 1] == wordTwo[wordTwoLength - 1]) {
+                return 1 + LongestStringSubsequence(
+                    wordOne.Substring(0, wordOneLength - 1),
+                    wordTwo.Substring(0, wordTwoLength - 1)
+                );
+            } else {
+                return Math.Max(
+                    LongestStringSubsequence(wordOne, wordTwo.Substring(0, wordTwoLength - 1)),
+                    LongestStringSubsequence(wordOne.Substring(0, wordOneLength - 1), wordTwo)
+                );
+            }
         }
     }
 }
