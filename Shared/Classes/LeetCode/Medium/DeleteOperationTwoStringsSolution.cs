a8928bfe301717019079aff3123d5052f5d1a5bd
GordeiU <gordey.usachev@gmail.com>
2022-06-14 22:00:14 +0100

    Day 8 Delete Operation Two Strings
    - Added the solution without the implementation of the algorithm core

--- Shared/Classes/LeetCode/Medium/DeleteOperationTwoStringsSolution.cs
+++ Shared/Classes/LeetCode/Medium/DeleteOperationTwoStringsSolution.cs
@@ -6,6 +6,13 @@ namespace Learning.Shared.Classes.LeetCode.Medium {
     /// </summary>
     public class DeleteOperationTwoStringsSolution {
         public int MinDistance(string wordOne, string wordTwo) {
+            int longestSubsequenceCount = LongestStringSubsequence(wordOne, wordTwo);
+
+            return wordOne.Length + wordTwo.Length - longestSubsequenceCount * 2;
+        }
+
+        private int LongestStringSubsequence(string wordOne, string wordTwo) {
+            //TODO: Implement longest string subsequence recursive? algorithm
             throw new NotImplementedException();
         }
     }
