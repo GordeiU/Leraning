654bc08ce46d44a1935bfcecc2891e702fb4e2bd
GordeiU <gordey.usachev@gmail.com>
2022-06-14 20:52:16 +0100

    Day 8 Delete Operation Two Strings
    - Added the signature of the method
    - Added unit test cases for the method

---
+++ Shared/Classes/LeetCode/Medium/DeleteOperationTwoStringsSolution.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Learning.Shared.Classes.LeetCode.Medium {
+    /// <summary>
+    /// Class <c>DeleteOperationTwoStringsSolution</c> Solution to the: https://leetcode.com/problems/delete-operation-for-two-strings/
+    /// </summary>
+    public class DeleteOperationTwoStringsSolution {
+        public int MinDistance(string wordOne, string wordTwo) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/LeetCode/Medium/DeleteOperationTwoStringsSolutionTest.cs
@@ -0,0 +1,34 @@
+using Learning.Shared.Classes.LeetCode.Medium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Learning.UnitTests.LeetCode.Easy {
+    [TestClass]
+    public class DeleteOperationTwoStringsSolutionTest {
+        private DeleteOperationTwoStringsSolution TestingClass { get; set; } = default!;
+
+        [TestInitialize]
+        public void TestInitialize() {
+            TestingClass = new DeleteOperationTwoStringsSolution();
+        }
+
+        [TestMethod]
+        public void MinDistance_ShortOne() {
+            string firstString = "sea";
+            string secondString = "eat";
+
+            int actual = TestingClass.MinDistance(firstString, secondString);
+            int expected = 2;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MinDistance_ShortTwo() {
+            string firstString = "leetcode";
+            string secondString = "etco";
+
+            int actual = TestingClass.MinDistance(firstString, secondString);
+            int expected = 4;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
