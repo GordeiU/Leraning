1f2ca57046054fb3a80fd27b8b6e5bb92ca7a5de
GordeiU <gordey.usachev@gmail.com>
2022-06-15 21:34:58 +0100

    Day 9 Longest String Chain
    - Added signature for the solution class
    - Added Unit tests for the algorithm

---
+++ Shared/Classes/LeetCode/Medium/LongestStrChainSolution.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Learning.Shared.Classes.LeetCode.Medium {
+    /// <summary>
+    /// Class <c>LongestStrChainSolution</c> Solution to the: https://leetcode.com/problems/longest-string-chain/
+    /// </summary>
+    public class LongestStrChainSolution {
+        public int LongestStrChain(string[] words) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/LeetCode/Medium/LongestStrChainSolutionTest.cs
@@ -0,0 +1,38 @@
+using Learning.Shared.Classes.LeetCode.Medium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Learning.UnitTests.LeetCode.Medium {
+    [TestClass]
+    public class LongestStrChainSolutionTest {
+        private LongestStrChainSolution TestingClass { get; set; } = default!;
+
+        [TestInitialize]
+        public void TestInitialize() {
+            TestingClass = new LongestStrChainSolution();
+        }
+
+        [TestMethod]
+        public void LongestStrChain_ShortOne() {
+            string[] words = new string[] { "a", "b", "ba", "bca", "bda", "bdca" };
+            int actual = TestingClass.LongestStrChain(words);
+            int expected = 4;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LongestStrChain_ShortTwo() {
+            string[] words = new string[] { "xbc", "pcxbcf", "xb", "cxbc", "pcxbc" };
+            int actual = TestingClass.LongestStrChain(words);
+            int expected = 5;
+            Assert.AreEqual(expected, actual);
+        }
+
+                [TestMethod]
+        public void LongestStrChain_ShortThree() {
+            string[] words = new string[] { "abcd", "dbqca" };
+            int actual = TestingClass.LongestStrChain(words);
+            int expected = 1;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
