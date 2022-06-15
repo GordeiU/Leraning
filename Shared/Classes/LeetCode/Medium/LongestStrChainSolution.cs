8820c864242c15f5d7292e365f241e9be4b477f6
GordeiU <gordey.usachev@gmail.com>
2022-06-15 22:38:10 +0100

    Day 9 Longest String Chain
    - Added algorithm solution is somewhat very slow as it needs to recalculate a lot of the same "word traversals". Need to ad memorisation technique to cut the complexity

--- Shared/Classes/LeetCode/Medium/LongestStrChainSolution.cs
+++ Shared/Classes/LeetCode/Medium/LongestStrChainSolution.cs
@@ -1,12 +1,52 @@
-using System;
+using System.Collections.Generic;
 
 namespace Learning.Shared.Classes.LeetCode.Medium {
     /// <summary>
     /// Class <c>LongestStrChainSolution</c> Solution to the: https://leetcode.com/problems/longest-string-chain/
     /// </summary>
     public class LongestStrChainSolution {
+        // TODO: Add the memorization
+        private HashSet<string>? vocabulary;
+
         public int LongestStrChain(string[] words) {
-            throw new NotImplementedException();
+            vocabulary = new HashSet<string>(words);
+
+            int result = -1;
+
+            foreach (string word in words) {
+                int intermediateResult = LongestContinuousSubsequence(word, 0);
+
+                if (intermediateResult > 0) {
+                    result = intermediateResult;
+                }
+
+            }
+
+            return result;
+        }
+
+        private int LongestContinuousSubsequence(string word, int count) {
+            if (word.Length == 1) {
+                return count + 1;
+            }
+
+            int longestChain = -1;
+
+            for (int idx = 0; idx < word.Length; idx++) {
+                string intermediateWord = word.Remove(idx, 1);
+
+                if (!vocabulary!.Contains(word)) {
+                    continue;
+                }
+
+                int subResult = LongestContinuousSubsequence(intermediateWord, count);
+
+                if (subResult > longestChain) {
+                    longestChain = subResult;
+                }
+            }
+
+            return longestChain + 1;
         }
     }
 }
