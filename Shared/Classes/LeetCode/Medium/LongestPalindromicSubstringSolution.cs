ddf336b01a9b6a433977dcf7dde336cbb09e6cba
GordeiU <gordey.usachev@gmail.com>
2022-06-16 23:47:35 +0100

    Day 9 Longest String Chain
    - Changed approach to the problem

--- Shared/Classes/LeetCode/Medium/LongestPalindromicSubstringSolution.cs
+++ Shared/Classes/LeetCode/Medium/LongestPalindromicSubstringSolution.cs
@@ -1,60 +1,47 @@
-using System.Collections.Generic;
-
 namespace Learning.Shared.Classes.LeetCode.Medium {
     /// <summary>
     /// Class <c>LongestPalindromicSubstringSolution</c> Solution to the: https://leetcode.com/problems/longest-palindromic-substring/
     /// </summary>
     public class LongestPalindromicSubstringSolution {
-        private HashSet<string> memory;
-
         private string LongestPalindromicSubstring { get; set; }
 
         private int Count => LongestPalindromicSubstring.Length;
 
         public LongestPalindromicSubstringSolution() {
             LongestPalindromicSubstring = string.Empty;
-            memory = new HashSet<string>();
-
-            memory.Add(string.Empty);
         }
 
         public string LongestPalindrome(string word) {
-            for (int wordLength = 1; wordLength <= word.Length; wordLength++) {
-                for (int startIdx = 0; startIdx <= word.Length - wordLength; startIdx++) {
-                    HasPalindrome(word.Substring(startIdx, wordLength));
+            int left;
+            int right;
+
+            for (int idx = 0; idx < word.Length; idx++) {
+                left = idx;
+                right = idx;
+
+                while (left >= 0 && right < word.Length && word[left] == word[right]) {
+                    if (right - left + 1 > Count) {
+                        LongestPalindromicSubstring = word.Substring(left, right - left + 1);
+                    }
+
+                    left--;
+                    right++;
+                }
+
+                left = idx;
+                right = idx + 1;
+
+                while (left >= 0 && right < word.Length && word[left] == word[right]) {
+                    if (right - left + 1 > Count) {
+                        LongestPalindromicSubstring = word.Substring(left, right - left + 1);
+                    }
+
+                    left--;
+                    right++;
                 }
             }
 
             return LongestPalindromicSubstring;
-        }
-
-        public bool HasPalindrome(string word) {
-            if (memory.Contains(word)) {
-                return true;
-            }
-
-            if (word.Length == 1) {
-                if (word.Length > Count) {
-                    LongestPalindromicSubstring = word;
-                }
-
-                memory.Add(word);
-                return true;
-            }
-
-            char prefix = word[0];
-            char postfix = word[word.Length - 1];
-
-            if (HasPalindrome(word.Substring(1, word.Length - 2)) && prefix == postfix) {
-                if (word.Length > Count) {
-                    LongestPalindromicSubstring = word;
-                }
-
-                memory.Add(word);
-                return true;
-            }
-
-            return false;
         }
     }
 }
