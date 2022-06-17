dc6867733d742992d2ae15e29db06f26c3cf5a93
GordeiU <gordey.usachev@gmail.com>
2022-06-17 21:56:42 +0100

    Day 10 Binary Tree Cameras
    - Added required Tree Node class
    - Added the signature of the solution

---
+++ Shared/Classes/LeetCode/Hard/DeleteOperationTwoStringsSolution.cs
@@ -0,0 +1,13 @@
+using System;
+using Learning.Shared.Classes.LeetCode.Classes;
+
+namespace Learning.Shared.Classes.LeetCode.Medium {
+    /// <summary>
+    /// Class <c>BinaryTreeCamerasSolution</c> Solution to the: https://leetcode.com/problems/binary-tree-cameras/
+    /// </summary>
+    public class BinaryTreeCamerasSolution {
+        public int MinCameraCover(TreeNode root) {
+            throw new NotImplementedException();
+        }
+    }
+}
