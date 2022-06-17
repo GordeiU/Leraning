76a3a83d895328982efac06f4cccf18b98c6dbb4
GordeiU <gordey.usachev@gmail.com>
2022-06-17 23:05:31 +0100

    Day 10 Binary Tree Cameras
    - Added a recursive BFS solution to the problem

--- Shared/Classes/LeetCode/Hard/BinaryTreeCamerasSolution.cs
+++ Shared/Classes/LeetCode/Hard/BinaryTreeCamerasSolution.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using Learning.Shared.Classes.LeetCode.Classes;
 
 namespace Learning.Shared.Classes.LeetCode.Hard {
@@ -6,10 +6,37 @@ namespace Learning.Shared.Classes.LeetCode.Hard {
     /// Class <c>BinaryTreeCamerasSolution</c> Solution to the: https://leetcode.com/problems/binary-tree-cameras/
     /// </summary>
     public class BinaryTreeCamerasSolution {
-        public int MinCameraCover(TreeNode root) {
-            TreeNode cur = root;
+        private HashSet<TreeNode?> covered;
+        private int cameraCount;
 
-            throw new NotImplementedException();
+        public BinaryTreeCamerasSolution() {
+            covered = new HashSet<TreeNode?>();
+            covered.Add(null);
+        }
+
+        public int MinCameraCover(TreeNode root) {
+            cameraCount = 0;
+
+            DepthFirstSearch(root, null);
+
+            return cameraCount;
+        }
+
+        private void DepthFirstSearch(TreeNode? node, TreeNode? parent) {
+            if (node != null) {
+                DepthFirstSearch(node.left, node);
+                DepthFirstSearch(node.right, node);
+
+                if (parent == null && !covered.Contains(node) ||
+                    !covered.Contains(node.left) ||
+                    !covered.Contains(node.right)) {
+                        cameraCount++;
+                        covered.Add(parent);
+                        covered.Add(node);
+                        covered.Add(node.left);
+                        covered.Add(node.right);
+                }
+            }
         }
     }
 }
