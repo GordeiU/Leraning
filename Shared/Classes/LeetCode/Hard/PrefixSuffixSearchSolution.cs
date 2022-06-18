f4e10a7d6c2eca8ac30f7cae759727d95ed50576
GordeiU <gordey.usachev@gmail.com>
2022-06-18 19:31:20 +0100

    Day 11 Prefix and Suffix Search
    - Added solution signature
    - Added unit tests

---
+++ Shared/Classes/LeetCode/Hard/PrefixSuffixSearchSolution.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Learning.Shared.Classes.LeetCode.Hard {
+    /// <summary>
+    /// Class <c>PrefixSuffixSearchSolution</c> Solution to the: https://leetcode.com/problems/prefix-and-suffix-search/
+    /// </summary>
+    public class PrefixSuffixSearchSolution {
+        public PrefixSuffixSearchSolution(string[] words) {
+
+        }
+
+        public int F(string prefix, string suffix) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/LeetCode/Hard/BinaryTreeCamerasSolutionTest copy.cs
@@ -0,0 +1,9 @@
+using Learning.Shared.Classes.LeetCode.Hard;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Learning.UnitTests.LeetCode.Hard {
+    [TestClass]
+    public class PrefixSuffixSearchSolutionTest {
+        private PrefixSuffixSearchSolution TestingClass { get; set; } = default!;
+    }
+}
