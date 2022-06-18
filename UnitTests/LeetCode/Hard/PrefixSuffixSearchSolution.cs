0e6a3a6ed4837ddc8a00c249217db879b22e72da
GordeiU <gordey.usachev@gmail.com>
2022-06-18 20:47:05 +0100

    Day 11 Prefix and Suffix Search
    - Renamed the file to match the class name

--- UnitTests/LeetCode/Hard/PrefixSuffixSearchSolution.cs
+++ UnitTests/LeetCode/Hard/PrefixSuffixSearchSolution.cs
@@ -9,37 +9,57 @@ namespace Learning.UnitTests.LeetCode.Hard {
         [TestMethod]
         public void CheckIfExists_OneWordExists() {
             string[] words = new string[] { "apple" };
-            PrefixSuffixSearchSolution solution = new PrefixSuffixSearchSolution(words);
+            PrefixSuffixSearchSolution TestingClass = new PrefixSuffixSearchSolution(words);
 
             int actual = TestingClass.F("a", "e");
             int expected = 0;
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod]
         public void CheckIfExists_OneWordDoesntExist_SuffixLongerThanWord() {
             string[] words = new string[] { "apple" };
-            PrefixSuffixSearchSolution solution = new PrefixSuffixSearchSolution(words);
+            PrefixSuffixSearchSolution TestingClass = new PrefixSuffixSearchSolution(words);
 
             int actual = TestingClass.F("a", "les");
             int expected = -1;
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod]
         public void CheckIfExists_OneWordDoesntExist_PrefixPartlyExists() {
             string[] words = new string[] { "apple" };
-            PrefixSuffixSearchSolution solution = new PrefixSuffixSearchSolution(words);
+            PrefixSuffixSearchSolution TestingClass = new PrefixSuffixSearchSolution(words);
 
             int actual = TestingClass.F("apd", "le");
             int expected = -1;
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod]
         public void CheckIfExists_TwoWordExists_SecondWordLonger() {
             // Both match but as per task should return longer word i.e apples
             string[] words = new string[] { "apple", "apples" };
-            PrefixSuffixSearchSolution solution = new PrefixSuffixSearchSolution(words);
+            PrefixSuffixSearchSolution TestingClass = new PrefixSuffixSearchSolution(words);
 
             int actual = TestingClass.F("ap", "le");
+            int expected = 1;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckIfExists_MoreWordExists() {
+            // Both match but as per task should return longer word i.e apples
+            string[] words = new string[] {
+                "Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing",
+                "elit", "Aenean", "venenatis", "convallis", "porttitor", "Integer",
+                "quam", "massa", "luctus", "sed", "pellentelesque", "et", "dictum",
+                "pellentelasdfesque", "pellenasdftelesque", "pellenasdftelesque"
+                };
+
+            PrefixSuffixSearchSolution TestingClass = new PrefixSuffixSearchSolution(words);
+
+            int actual = TestingClass.F("pel", "que");
             int expected = 1;
             Assert.AreEqual(expected, actual);
         }
