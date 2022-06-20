b2ffd324244808f64da85461a2ff8238977122fc
GordeiU <gordey.usachev@gmail.com>
2022-06-20 22:47:10 +0100

    Day 12 Search Suggestions System
    - Added signatures for the solution method
    - Added unit test
    (Not finished have to revisit)

---
+++ Shared/Classes/LeetCode/Medium/SearchSuggestionsSystemSolution.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Learning.Shared.Classes.LeetCode.Medium {
+    /// <summary>
+    /// Class <c>SearchSuggestionsSystemSolution</c> Solution to the: https://leetcode.com/problems/search-suggestions-system/
+    /// </summary>
+    public class SearchSuggestionsSystemSolution {
+        public List<List<string>> SuggestedProducts(string[] products, string searchWord) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/LeetCode/Medium/SearchSuggestionsSystemSolutionTest.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using Learning.Shared.Classes.LeetCode.Medium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Learning.UnitTests.LeetCode.Medium {
+    [TestClass]
+    public class SearchSuggestionsSystemSolutionTest {
+        private SearchSuggestionsSystemSolution TestingClass { get; set; } = default!;
+
+        [TestInitialize]
+        public void TestInitialize() {
+            TestingClass = new SearchSuggestionsSystemSolution();
+        }
+
+        [TestMethod]
+        public void MinDistance_ShortOne() {
+            string[] words = new string[] { "mobile", "mouse", "moneypot", "monitor", "mousepad" };
+            bool isEqual = false;
+
+            List<List<string>> actual = TestingClass.SuggestedProducts(words, "mouse");
+            List<List<string>> expected = new List<List<string>>() {
+                new List<string>() {"mobile", "moneypot", "monitor"},
+                new List<string>() {"mobile", "moneypot", "monitor"},
+                new List<string>() {"mouse", "mousepad"},
+                new List<string>() {"mouse", "mousepad"},
+                new List<string>() {"mouse", "mousepad"}
+            };
+
+            if (actual.Count != expected.Count) {
+                isEqual = false;
+            }
+
+            for (int idx = 0; idx < expected.Count; idx++) {
+                isEqual = isEqual && Enumerable.SequenceEqual(actual[idx], expected[idx]);
+            }
+
+            Assert.IsTrue(isEqual);
+        }
+    }
+}
