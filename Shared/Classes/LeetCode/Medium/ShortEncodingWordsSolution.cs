513a07ed51031a23a17e47c8aae314ca10806095
GordeiU <gordey.usachev@gmail.com>
2022-06-20 22:50:34 +0100

    Day 13 Short Encoding of Words
    - Added a solution signature
    - Added unit tests

---
+++ Shared/Classes/LeetCode/Medium/ShortEncodingWordsSolution.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Learning.Shared.Classes.LeetCode.Medium {
+    /// <summary>
+    /// Class <c>ShortEncodingWordsSolution</c> Solution to the: https://leetcode.com/problems/short-encoding-of-words/
+    /// </summary>
+    public class ShortEncodingWordsSolution {
+        public int MinimumLengthEncoding(string[] words) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/LeetCode/Medium/ShortEncodingWordsSolutionTest.cs
@@ -0,0 +1,23 @@
+using Learning.Shared.Classes.LeetCode.Medium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Learning.UnitTests.LeetCode.Medium {
+    [TestClass]
+    public class ShortEncodingWordsSolutionTest {
+        private ShortEncodingWordsSolution TestingClass { get; set; } = default!;
+
+        [TestInitialize]
+        public void TestInitialize() {
+            TestingClass = new ShortEncodingWordsSolution();
+        }
+
+        [TestMethod]
+        public void MinDistance_ShortOne() {
+            string[] words = new string[] {"time", "me", "bell" };
+
+            int actual = TestingClass.MinimumLengthEncoding(words);
+            int expected = 10;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
