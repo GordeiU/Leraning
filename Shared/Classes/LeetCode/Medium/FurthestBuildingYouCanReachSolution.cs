fc4b0e20be576fc5ef7a3a2b46f2bb575288f77b
GordeiU <gordey.usachev@gmail.com>
2022-06-21 22:36:34 +0100

    Day 14 Furthest Building You Can Reach
    - Added priority queue implementation (fails the test cases on the LeetCode)

--- Shared/Classes/LeetCode/Medium/FurthestBuildingYouCanReachSolution.cs
+++ Shared/Classes/LeetCode/Medium/FurthestBuildingYouCanReachSolution.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Learning.Shared.Classes.LeetCode.Medium {
     /// <summary>
@@ -6,7 +7,35 @@ namespace Learning.Shared.Classes.LeetCode.Medium {
     /// </summary>
     public class FurthestBuildingYouCanReachSolution {
         public int FurthestBuilding(int[] heights, int bricks, int ladders) {
-            throw new NotImplementedException();
+            PriorityQueue<int, int> queue = new PriorityQueue<int, int>();
+            int gap;
+
+            for (int idx = 0; idx < heights.Length - 1; idx++) {
+                gap = heights[idx + 1] - heights[idx];
+
+                if (gap <= 0) {
+                    continue;
+                }
+                
+                if (ladders == 0 && bricks < gap) {
+                    return idx;
+                }
+
+                if (gap <= bricks) {
+                    bricks -= gap;
+                    queue.Enqueue(gap, gap);
+                    continue;
+                }
+
+                if (queue.Count > 0) {
+                    bricks += queue.Dequeue();
+                }
+
+                ladders--;
+                idx--;
+            }
+
+            return heights.Length;
         }
     }
 }
