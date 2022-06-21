43fbc5d784d88541d408117609892e04a1b7db2e
GordeiU <gordey.usachev@gmail.com>
2022-06-21 22:56:38 +0100

    Day 14 Furthest Building You Can Reach
    - Failed test added to the unit tests

--- UnitTests/LeetCode/Medium/FurthestBuildingYouCanReachSolutionTest.cs
+++ UnitTests/LeetCode/Medium/FurthestBuildingYouCanReachSolutionTest.cs
@@ -21,5 +21,16 @@ namespace Learning.UnitTests.LeetCode.Medium {
             int expected = 4;
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void FurthestBuilding_ShortTwo() {
+            int[] buildingHeights = new int[] { 4, 12, 2, 7, 3, 18, 20, 3, 19 };
+            int bricks = 10;
+            int ladders = 2;
+
+            int actual = TestingClass.FurthestBuilding(buildingHeights, bricks, ladders);
+            int expected = 7;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
